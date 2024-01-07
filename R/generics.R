b2bce670635e20e2fae9446f54048466c60b160f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-07 01:06:16 -0500

    docs

--- R/generics.R
+++ R/generics.R
@@ -1,14 +1,67 @@
+#' Style Cells in a `tinytable`
+#'
+#' @description
+#' This function applies styling to specified parts of a table created by the `tt()` function of the `tinytable` package.
+#'
+#' The arguments described below allow users to customize text style and color. The `latex` and `html` arguments allow deep customization using the `tabularray` package for LaTeX, and the Bootstrap framework for HTML. For details, see:
+#' * `?latexOptions`
+#' * `?htmlOptions`
+#'
+#' @param x A table created by the `tt()` function.
+#' @param i Integer vector indicating row positions. If `NULL`, applies to all rows.
+#' @param j Integer vector indicating column positions. If `NULL`, applies to all columns.
+#' @param color String specifying the text color. If `NULL`, no color is applied.
+#' @param background String specifying the background color. If `NULL`, no background is applied.
+#' @param bold Logical; if `TRUE`, applies bold styling to the text.
+#' @param italic Logical; if `TRUE`, applies italic styling to the text.
+#' @param latex sytling options for the `tabularray` framework for LaTeX. See `?latexOptions` for details.
+#' @param html styling options for the Bootstrap framework for HTML. See `?htmlOptions` for details.
+#' @return Returns the modified table object with the specified cell styles applied.
 #' @export
-style_cell <- function(x, i, j, color, background, bold, italic, latex, html) {
+style_cell <- function(x,
+                       i = NULL,
+                       j = NULL,
+                       color = NULL,
+                       background = NULL,
+                       bold = FALSE,
+                       italic = FALSE,
+                       latex = latexOptions(),
+                       html = htmlOptions()) {
   UseMethod("style_cell", x)
 }
 
+
+#' Style Rows in a `tinytable`
+#'
+#' @inheritParams style_cell
+#' @inherit style_cell return
+#' @inherit style_cell description
 #' @export
-style_row <- function(x, i, color, background, bold, italic, latex, html) {
+style_row <- function(x,
+                      i = NULL,
+                      color = NULL,
+                      background = NULL,
+                      bold = FALSE,
+                      italic = FALSE,
+                      latex = latexOptions(),
+                      html = htmlOptions()) {
   UseMethod("style_row", x)
 }
 
+
+#' Style Columns in a `tinytable`
+#'
+#' @inheritParams style_cell
+#' @inherit style_cell return
+#' @inherit style_cell description
 #' @export
-style_column <- function(x, i, align, color, background, bold, italic, latex, html) {
+style_column <- function(x,
+                         j = NULL,
+                         color = NULL,
+                         background = NULL,
+                         bold = FALSE,
+                         italic = FALSE,
+                         latex = latexOptions(),
+                         html = htmlOptions()) {
   UseMethod("style_column", x)
 }
