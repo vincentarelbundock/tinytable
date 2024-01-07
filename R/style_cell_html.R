8fb0c8833c0132b49556b1c333901160041d221a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-07 16:14:56 -0500

    get_started.qmd

--- R/style_cell_html.R
+++ R/style_cell_html.R
@@ -22,19 +22,36 @@ style_cell.tinytable_html <- function(x,
 
   out <- x
 
+  cellid <- get_id(stem = "style__row_")
+
+  css <- sprintf("%s .table td.%s {", strrep(" ", 8), cellid)
+  if (!is.null(color)) {
+    tmp <- sprintf("%s color: %s", strrep(" ", 10), color)
+    css <- c(css, tmp)
+  }
+  if (!is.null(background)) {
+    tmp <- sprintf("%s background-color: %s", strrep(" ", 10), background)
+    css <- c(css, tmp)
+  }
+  if (isTRUE(bold)) {
+    tmp <- sprintf("%s font-weight: bold", strrep(" ", 10))
+    css <- c(css, tmp)
+  }
+  if (isTRUE(italic)) {
+    tmp <- sprintf("%s font-style: italic", strrep(" ", 10))
+    css <- c(css, tmp)
+  }
+  tmp <- sprintf("%s}", strrep(" ", 8))
+  css <- c(css, tmp)
+  css <- trimws(css)
+  css <- paste(css, collapse = "; ")
+
+  out <- bootstrap_setting(out, css, component = "css")
+
   for (row in i) {
     for (col in j) {
-
-      if (!is.null(color)) {
-        new <- sprintf('table.rows[%s].cells[%s].style.backgroundColor = "%s";', row, col, color)
-        out <- bootstrap_setting(out, new, component = "cell")
-      }
-
-      if (!is.null(background)) {
-        new <- sprintf('table.rows[%s].cells[%s].style.backgroundColor = "%s";', row, col, background)
-        out <- bootstrap_setting(out, new, component = "cell")
-      }
-
+      new <- sprintf("table.rows[%s].cells[%s].classList.add('%s');", row, col, cellid)
+      out <- bootstrap_setting(out, new, component = "row")
     }
   }
 
