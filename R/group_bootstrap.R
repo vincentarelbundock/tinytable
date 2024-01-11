f71bb44930d8c846ca7962e6063e12769c6de3ff
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-11 16:56:33 -0500

    TODO: group_bootstrap() and fix colspan for bootstrap using JS

--- R/group_bootstrap.R
+++ R/group_bootstrap.R
@@ -1,9 +1,7 @@
-
-
 group_bootstrap <- function(x, i = TRUE, italic = TRUE, rule = TRUE, indent = 1) {
-  out <- bootstrap_setting(x, "let row = table.insertRow(1);", component = "newrows")
-  out <- bootstrap_setting(out, "let c1 = row.insertCell(0);", component = "newrows")
-  out <- bootstrap_setting(out, "c1.innerText = 'Blah'", component = "newrows")
-  out <- bootstrap_setting(out, "table.rows[0].cells[0].setAttribute('colspan', 8);")
+  js <- sprintf(
+    "window.addEventListener('load', function () { spanCell(%s, %s, '%s') }",
+    i, ncol(x), x[i, 1])
+  out <- bootstrap_setting(x, js, component = "cells")
   return(out)
 }
