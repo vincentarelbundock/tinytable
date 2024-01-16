fa97312b3754bfd477c47c848e3e21285a9961ee
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-16 18:43:15 -0500

    cruft

--- R/print.R
+++ R/print.R
@@ -6,9 +6,6 @@ knit_print.tinytable <- function(x, ...) {
   # lazy styles get evaluated here, at the very end
   # not sure why we need to call this twice, but it appears necessary
   out <- eval_style(x)
-if (condition) {
-    
-}
   if (meta(out)$output == "html") {
     # from htmltools:::html_preserve
     # GPL3
