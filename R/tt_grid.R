7524847b74c2291e3667ed27bfcc4f3bf0e8ff01
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-19 16:19:42 -0500

    group_grid_row

--- R/tt_grid.R
+++ R/tt_grid.R
@@ -84,7 +84,24 @@ empty_cells <- function(lst) {
 }
 
 
-group_grid <- function(x, j, ...) {
+group_grid <- function(x, i = NULL, j = NULL, ...) {
+  out <- x
+
+  if (!is.null(i)) {
+    out <- group_grid_row(out, i)
+  }
+
+  if (!is.null(j)) {
+    out <- group_grid_col(out, j)
+  }
+
+  return(out)
+}
+
+
+group_grid_col <- function(x, j, ...) {
+
+  # columns
   header <- empty_cells(j)
   cw <- meta(x, "col_widths")
   cw <- sapply(header, function(k) sum(cw[k]) + length(cw[k]) - 1)
@@ -101,12 +118,37 @@ group_grid <- function(x, j, ...) {
 }
 
 
-df <- data.frame(
-  Fruit = c("Bananas", "Oranges"),
-  Price = c("$1.34", "$2.10"),
-  Features = c(2, 4.3),
-  Color = c("Yellow", "Orange"),
-  Smell = c("Bad", "Good")
-)
+
+group_grid_row <- function(x, i, ...) {
+  out <- x
+  out <- strsplit(x, split = "\\n")[[1]]
+  # header
+  body_min <- utils::head(grep("^\\+==", out), 1) + 1
+  # no header
+  if (is.na(body_min)) {
+    body_min <- utils::head(grep("^\\+--", out), 1) + 1
+  }
+  body_max <- utils::tail(grep("^\\+--", out), 1) - 1
+  body <- body_min:body_max
+  top <- out[1:(min(body) - 1)]
+  mid <- out[min(body):max(body)]
+  bot <- out[(max(body) + 1):length(out)]
+
+  cw <- meta(x, "col_widths")
+  cw <- sum(cw) + length(cw) - 1
+  for (idx in rev(seq_along(i))) {
+    tmp = trimws(as.character(tt_grid(matrix(names(i)[idx]), col_widths = cw)))
+    mid <- c(mid[1:(i[idx] - 1)], tmp, mid[i[idx]:length(body)]) 
+  }
+
+  out <- c(top, mid, bot)
+  out <- paste(out, collapse = "\n")
+
+  attr(out, "tinytable_meta") <- meta(x)
+  class(out) <- class(x)
+
+  return(out)
+}
+
 
 
--- inst/tinytest/test-markdown.R
+++ inst/tinytest/test-markdown.R
@@ -9,5 +9,16 @@ expect_snapshot_print(
   tt(k), label = "markdown-nocolnames")
 
 
+tab <- tt(mtcars[1:10, 1:5]) |>
+    group_tt(
+      i = list(
+        "Hello" = 3,
+        "World" = 8),
+      j = list(
+        "Foo" = 2:3,
+        "Bar" = 4:5))
+
+expect_snapshot_print(tab, label = "markdown-group_tt")
+
 
 options(tinytable_print_output = NULL)
