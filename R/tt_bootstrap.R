7d5e8352a355f37e71c1eff2e4d26b7e372657b5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-20 16:19:59 -0500

    minor

--- R/tt_bootstrap.R
+++ R/tt_bootstrap.R
@@ -66,6 +66,7 @@ tt_bootstrap <- function(x, caption, theme, width, notes, ...) {
   template <- gsub("styleCell", paste0("styleCell_", id), template, fixed = TRUE)
   template <- gsub("styleHeaderCell", paste0("styleHeaderCell_", id), template, fixed = TRUE)
   template <- gsub("$tinytable_TABLE_ID", paste0("tinytable_", id), template, fixed = TRUE)
+  browser()
 
   # header
   idx <- grep("$tinytable_BOOTSTRAP_HEADER", template, fixed = TRUE)
