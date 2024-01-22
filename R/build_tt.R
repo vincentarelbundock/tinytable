46e4bc6e768b7e5c523a9b10039ee9a381b5edc9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-21 21:42:38 -0500

    issue #87

--- R/build_tt.R
+++ R/build_tt.R
@@ -97,5 +97,11 @@ finalize_bootstrap <- function(x) {
       "table",
       x,
       fixed = TRUE)
+    # Rmarkdown and Quarto load their own bootstrap, which we probably don't want to override
+    if (isTRUE(getOption('knitr.in.progress'))) {
+      out <- strsplit(out, split = "\n")[[1]]
+      out <- out[!grepl("https://cdn.jsdelivr.net/npm/bootstrap", out, fixed = TRUE)]
+      out <- paste(out, collapse = "\n")
+    }
     return(out)
 }
