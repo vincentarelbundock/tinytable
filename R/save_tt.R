09890a2b247750b440612988c01bc98c907a5f7f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-01-29 07:38:07 -0500

    docs

--- R/save_tt.R
+++ R/save_tt.R
@@ -4,9 +4,8 @@
 #'
 #' @param x The tinytable object to be saved.
 #' @param output String or file path. 
-#' + If `output` is "markdown", "latex", or "html", the table is returned in a string as an `R` object. 
-#' + If `output` is a valid file path, the table is saved to file. The supported extensions are: .docx, .html, .png, .pdf, .tex and .md (with aliases .txt, .Rmd and .qmd).
-#' + Warning: `style_tt()` does not work for Markdown or Word tables.
+#' + If `output` is "markdown", "latex", "html", or "typst", the table is returned in a string as an `R` object. 
+#' + If `output` is a valid file path, the table is saved to file. The supported extensions are: .docx, .html, .png, .pdf, .tex, .typ, and .md (with aliases .txt, .Rmd and .qmd).
 #' @param overwrite A logical value indicating whether to overwrite an existing file. 
 #' @return A string or `TRUE` when the table is written to file.
 #' @export
