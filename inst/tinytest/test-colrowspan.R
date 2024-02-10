ecff9a1eec658dbe22723a5f5c8e9fe21b5044f0
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-02-10 07:46:30 -0500

    no dplyr

--- inst/tinytest/test-colrowspan.R
+++ inst/tinytest/test-colrowspan.R
@@ -6,12 +6,9 @@ requiet("dplyr")
 
 
 options(tinytable_print_output = "markdown")
-tab <- mtcars |>
-    summarize(mpg = mean(mpg), .by = c("cyl", "am")) |>
-    arrange(cyl, am)
+tab <- aggregate(mpg ~ cyl + am, FUN = mean, data = mtcars)
+tab <- tab[order(tab$cyl, tab$am),]
 tab <- tt(tab, digits = 2) |>
-    style_tt(i = 1, j = 1, rowspan = 2, colspan = 2) |>
-    style_tt(i = 3, j = 1, rowspan = 2, colspan = 2) |>
-    style_tt(i = 5, j = 1, rowspan = 2, colspan = 2)
+    style_tt(i = c(1, 3, 5), j = 1, rowspan = 2, colspan = 2)
 expect_snapshot_print(tab, "colrowspan-markdown_multiple")
 options(tinytable_print_output = NULL)
