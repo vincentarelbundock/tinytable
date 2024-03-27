f0ecd5146b262b9ea1029f9f35deb090a4bb25a6
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-03-27 14:55:37 -0400

    test

---
+++ inst/tinytest/test-print.R
@@ -0,0 +1,23 @@
+source("helpers.R")
+using("tinysnapshot")
+
+tab <- tt(mtcars[1:4, 1:5])
+
+options("tinytable_print_output" = "markdown")
+void <- capture.output(x <- print(tab))
+expect_equivalent(x@output, "markdown")
+
+options("tinytable_print_output" = "latex")
+void <- capture.output(x <- print(tab))
+expect_equivalent(x@output, "latex")
+
+options("tinytable_print_output" = "typst")
+void <- capture.output(x <- print(tab))
+expect_equivalent(x@output, "typst")
+
+# avoid launching a viewer
+# options("tinytable_print_output" = "html")
+# void <- capture.output(x <- print(tab))
+# expect_equivalent(x@output, "html")
+
+options("tinytable_print_output" = NULL)
