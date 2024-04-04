7eddf148922806c388d8868bf13583667bb76e1c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-04-04 11:19:37 -0400

    white space + useless file

--- R/style_typst.R
+++ R/style_typst.R
@@ -26,68 +26,68 @@ setMethod(
                         indent = 0,
                         midrule = FALSE, # undocumented, only used by `group_tt()`
                         ...) {
-    
+
   out <- x@table_string
-    
+
   ival <- if (is.null(i)) seq_len(nrow(x)) else i
   jval <- if (is.null(j)) seq_len(ncol(x)) else j
-    
+
   # only columns means we also want to style headers
   if (is.null(i) && !is.null(j)) {
     ival <- c(-1 * rev(seq_len(x@nhead) - 1), ival)
   }
-    
+
   # 0- & header-indexing
   jval <- jval - 1
   ival <- ival - 1 + x@nhead
-    
+
   if (isTRUE(grepl("^#", color))) color <- sprintf('rgb("%s")', color)
   if (isTRUE(grepl("^#", background))) background <- sprintf('rgb("%s")', background)
   if (isTRUE(grepl("^#", line_color))) line_color <- sprintf('rgb("%s")', line_color)
-    
+
   style <- ""
-  
+
   if (!is.null(fontsize)) {
     tmp <- sprintf("if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = { set text(%sem); cell.content } };", fontsize)
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (isTRUE(monospace)) {
     tmp <- "if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = math.mono(cell.content) };"
     style <- paste0(style, "\n", tmp)
   }
-  
+
   if (isTRUE(italic)) {
     tmp <- "if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = emph(cell.content) };"
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (isTRUE(bold)) {
     tmp <- "if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = strong(cell.content) };"
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (isTRUE(underline)) {
     tmp <- "if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = underline(cell.content) };"
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (isTRUE(strikeout)) {
     tmp <- "if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = strike(cell.content) };"
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (!is.null(background)) {
     tmp <- sprintf("if (i.contains(cell.y) and j.contains(cell.x)) { cell.fill = %s };", background)
     style <- paste0(style, "\n", tmp)
   }
-    
+
   if (!is.null(color)) {
     tmp <- sprintf("if (i.contains(cell.y) and j.contains(cell.x)) { cell.content = { set text(%s); cell.content } };", color)
     style <- paste0(style, "\n", tmp)
   }
-    
-    
+
+
   if (style != "") {
     idx <- sprintf(
       "let i = (%s,);
@@ -96,9 +96,9 @@ let j = (%s,);",
       paste(jval, collapse = ","))
       style <- paste0(idx, "\n", style)
   }
-    
+
   out <- typst_insert(out, style, type = "style")
-    
+
   # align
   if (!is.null(align)) {
     for (idx in seq_along(jval)) {
@@ -113,8 +113,8 @@ let j = (%s,);",
       out <- typst_insert(out, tmp, type = "style")
     }
   }
-  
-    
+
+
   # Lines are not part of cellspec/rowspec/columnspec. Do this separately.
   if (!is.null(line)) {
     iline <- NULL
@@ -135,7 +135,7 @@ let j = (%s,);",
                      line_color)
       out <- typst_insert(out, tmp, type = "lines")
       }
-      
+
     jline <- NULL
     if (grepl("r", line)) jline <- c(jline, jval + 1)
     if (grepl("l", line)) jline <- c(jline, jval)
@@ -150,10 +150,10 @@ let j = (%s,);",
         line_color)
       out <- typst_insert(out, tmp, type = "lines")
     }
-    
+
   }
-  
+
   x@table_string <- out
-  
+
   return(x)
 })
