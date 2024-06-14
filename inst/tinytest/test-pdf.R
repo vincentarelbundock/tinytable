71b31500984966c7e4e6298a6cfadda9dbd6292e
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-06-14 11:34:15 +0100

    Automatically remove logfiles generated by tinytex

---
+++ inst/tinytest/test-pdf.R
@@ -0,0 +1,34 @@
+source("helpers.R")
+
+x <- cbind(mtcars[1, ], mtcars[1, ])
+dest_dir <- tempdir()
+dest_pdf <- file.path(dest_dir, "out.pdf")
+dest_log <- file.path(dest_dir, "somelog.log")
+
+expect_warning(
+    tt(x) |>
+        theme_tt("resize") |>
+        save_tt(dest_pdf),
+    "Table width is too small"
+)
+
+# logfiles are automatically deleted
+expect_equal(
+    length(grep("\\.log$", list.files(dest_dir))),
+    0
+)
+
+# logfiles that exist before call to tinytex as left untouched
+cat("some content", file = dest_log)
+
+expect_warning(
+    tt(x) |>
+        theme_tt("resize") |>
+        save_tt(dest_pdf, overwrite = TRUE),
+    "Table width is too small"
+)
+
+expect_true("somelog.log" %in% list.files(dest_dir))
+
+# Clean slate
+suppressWarnings(invisible(file.remove(list.files(dest_dir))))
