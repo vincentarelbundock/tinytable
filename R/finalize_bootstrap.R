57c8bfcd6c24976ef78af72f6f121222ce8d7459
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-12 13:36:09 +0200

    cruft

--- R/finalize_bootstrap.R
+++ R/finalize_bootstrap.R
@@ -32,17 +32,6 @@ setMethod(
   out <- gsub("styleCell_\\w+\\(", paste0("styleCell_", x@id, "("), out)
   out <- gsub("spanCell_\\w+\\(", paste0("spanCell_", x@id, "("), out)
 
-  ## I would like to use this fancy listener, but I can't get it to work with multiple style_tt()/group_tt() calls
-  # # CSS listeners
-  # listener_template <- "
-  #  window.addEventListener('load', function () {
-  #      const cellStyles = [
-  #          %s
-  #      ];
-  #      cellStyles.forEach(({coords, class: cssClass}) => {
-  #          styleCell_%s('%s', coords, cssClass);
-  #      });
-  #  });"
 
   css_template <- "    .table td.%s, .table th.%s { %s }"
 
@@ -81,22 +70,6 @@ setMethod(
                     css_rules_unique$bootstrap[[ii]])
         out <- bootstrap_setting(out, css_rule, component = "css")
     }
-
-  ## I would like to use the fancier listener, but I can't get it to work.
-    # css_rules <- split(css_rules, css_rules$id)
-    # coords <- lapply(css_rules, function(z) sprintf("[%s, %s]", z$i, z$j))
-    # coords <- lapply(coords, paste, collapse = ", ")
-  #   for (i in seq_along(coords)) {
-  #       co <- sprintf(
-  #         "{coords: [%s], class: '%s'},", 
-  #         coords[[i]],
-  #         names(coords)[i])
-  #       listener <- sprintf(listener_template, co, x@id, names(coords)[i])
-  #       out <- bootstrap_setting(out, listener, component = "cell")
-  #       css_rule <- sprintf(css_template, names(css_rules)[i], names(css_rules)[i], css_rules[[i]]$bootstrap[1])
-  #       out <- bootstrap_setting(out, css_rule, component = "css")
-  #   }
-  #
 
   }
 
