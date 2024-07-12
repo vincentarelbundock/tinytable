d9943514b11f01bfb262fa8c47230566bbd11350
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-12 08:55:00 +0200

    still broken

--- R/finalize_bootstrap.R
+++ R/finalize_bootstrap.R
@@ -32,16 +32,17 @@ setMethod(
   out <- gsub("styleCell_\\w+\\(", paste0("styleCell_", x@id, "("), out)
   out <- gsub("spanCell_\\w+\\(", paste0("spanCell_", x@id, "("), out)
 
-  # CSS listeners
-  listener_template <- "
-   window.addEventListener('load', function () {
-       const cellStyles = [
-           %s
-       ];
-       cellStyles.forEach(({coords, class: cssClass}) => {
-           styleCell_%s('%s', coords, cssClass);
-       });
-   });"
+  ## I would like to use this fancy listener, but I can't get it to work with multiple style_tt()/group_tt() calls
+  # # CSS listeners
+  # listener_template <- "
+  #  window.addEventListener('load', function () {
+  #      const cellStyles = [
+  #          %s
+  #      ];
+  #      cellStyles.forEach(({coords, class: cssClass}) => {
+  #          styleCell_%s('%s', coords, cssClass);
+  #      });
+  #  });"
 
   css_template <- "    .table td.%s, .table th.%s { %s }"
 
@@ -49,25 +50,53 @@ setMethod(
   css <- css[which(css$bootstrap != ""), ]
 
   if (nrow(css) > 0) {
-    css_rules <- split(css, list(css$i, css$j, css$id))
-    css_rules <- Filter(function(z) nrow(z) > 0, css_rules)
-    css_rules <- lapply(css_rules, function(z) transform(z, bootstrap = paste(bootstrap)))
-    css_rules <- lapply(css_rules, unique)
-    css_rules <- do.call(rbind, css_rules)
-    css_rules <- split(css_rules, css_rules$id)
-    coords <- lapply(css_rules, function(z) sprintf("[%s, %s]", z$i, z$j))
-    coords <- lapply(coords, paste, collapse = ", ")
 
-    for (i in seq_along(coords)) {
-        co <- sprintf(
-          "{coords: [%s], class: '%s'},", 
-          coords[[i]],
-          names(coords)[i])
-        listener <- sprintf(listener_template, co, x@id, names(coords)[i])
+    css_rules <- css
+    css_rules$id <- NULL
+    css_rules <- split(css_rules, list(css_rules$i, css_rules$j))
+    css_rules <- Filter(function(z) nrow(z) > 0, css_rules)
+    css_rules <- lapply(css_rules, function(z) z[rev(seq_len(nrow(z))),])
+    css_rules <- lapply(css_rules, unique)
+    css_rules <- lapply(css_rules, function(z) transform(z, bootstrap = paste(bootstrap, collapse = " "))[1,])
+    css_rules <- do.call(rbind, css_rules)
+    id <- unique(css_rules[, "bootstrap", drop = FALSE])
+    id$id <- sapply(seq_len(nrow(id)), function(z) sprintf("tinytable_css_%s", get_id()))
+    css_rules <- merge(css_rules, id)
+    css_rules <- css_rules[order(css_rules$i, css_rules$j),]
+
+    for (ii in seq_len(nrow(css_rules))) {
+        listener <- sprintf(
+            "window.addEventListener('load', function () { styleCell_tinytable_(%s, %s, '%s') })",
+            css_rules$i[[ii]],
+            css_rules$j[[ii]],
+            css_rules$id[[ii]])
         out <- bootstrap_setting(out, listener, component = "cell")
-        css_rule <- sprintf(css_template, names(css_rules)[i], names(css_rules)[i], css_rules[[i]]$bootstrap[1])
+    }
+    css_rules_unique <- unique(css_rules[, c("bootstrap", "id")])
+    for (ii in seq_len(nrow(css_rules_unique))) {
+        css_rule <- sprintf(css_template, 
+                    css_rules_unique$id[[ii]],
+                    css_rules_unique$id[[ii]],
+                    css_rules_unique$bootstrap[[ii]])
         out <- bootstrap_setting(out, css_rule, component = "css")
     }
+
+  ## I would like to use the fancier listener, but I can't get it to work.
+    # css_rules <- split(css_rules, css_rules$id)
+    # coords <- lapply(css_rules, function(z) sprintf("[%s, %s]", z$i, z$j))
+    # coords <- lapply(coords, paste, collapse = ", ")
+  #   for (i in seq_along(coords)) {
+  #       co <- sprintf(
+  #         "{coords: [%s], class: '%s'},", 
+  #         coords[[i]],
+  #         names(coords)[i])
+  #       listener <- sprintf(listener_template, co, x@id, names(coords)[i])
+  #       out <- bootstrap_setting(out, listener, component = "cell")
+  #       css_rule <- sprintf(css_template, names(css_rules)[i], names(css_rules)[i], css_rules[[i]]$bootstrap[1])
+  #       out <- bootstrap_setting(out, css_rule, component = "css")
+  #   }
+  #
+
   }
 
   x@table_string <- out
