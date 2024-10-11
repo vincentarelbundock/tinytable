7e0da6e0fc672ce9e1af2074752ffe5e286abc0d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-10-11 10:13:37 -0400

    test

--- inst/tinytest/test-typst.R
+++ inst/tinytest/test-typst.R
@@ -118,8 +118,8 @@ expect_snapshot_print(tab, label = "typst-issue323_group_tt_style_tt")
 
 
 # Issue #357
-pkgload::load_all()
-tt(head(iris), notes = "blah") |> print("typst")
+tab <- tt(head(iris), notes = "blah") |> save_tt("typst")
+expect_true(grepl('blah', tab))
 
 
 options(tinytable_print_output = NULL)
