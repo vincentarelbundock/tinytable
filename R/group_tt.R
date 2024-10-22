2a3bb9a2be0aab89150157b7ed81ed59eec57c00
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-10-21 21:51:00 -0400

    group_tt

--- R/group_tt.R
+++ R/group_tt.R
@@ -75,8 +75,7 @@ group_tt <- function(x, i = NULL, j = NULL, indent = 1, ...) {
       stop("Only one row-wise `group_tt(i = ...)` call is allowed.", call. = FALSE)
     }
     x@group_tt_i <- TRUE
-    idx_indent <- i + cumsum(rep(1, length(i))) - 1
-    idx_indent <- setdiff(seq_len(nrow(x) + length(i)), idx_indent)
+    idx_indent <- setdiff(seq_len(nrow(x) + length(i)), i)
     x <- style_tt(x, i = idx_indent, j = 1, indent = indent)
   }
 
