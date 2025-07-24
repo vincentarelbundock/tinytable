11e7593bbd43a486cf47b3a632bbde5012d6835d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-07-24 15:57:42 -0400

    test-notes.R snapshot

--- inst/tinytest/test-notes.R
+++ inst/tinytest/test-notes.R
@@ -6,7 +6,7 @@ options(tinytable_print_output = "latex")
 x <- mtcars[1:3, 1:3]
 n <- list("Blah blah", "*" = list(i = 0:1, j = 2, text = "foo bar"))
 tab <- tt(x, notes = n)
-expect_snapshot_print(tab, label = "notes-latex_cell_markers")
+expect_snapshot_print(tab, label = "notes-latex_cell_markers.tex")
 options(tinytable_print_output = NULL)
 
 # s4 refactor bug along the way
