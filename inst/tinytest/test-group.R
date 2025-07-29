1bcbdf4839932547bb20b3c6a59cba032ab34ce8
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-07-28 21:36:16 -0400

    test rename

--- inst/tinytest/test-group.R
+++ inst/tinytest/test-group.R
@@ -10,7 +10,7 @@ dat <- data.frame(
 )
 tab <- tt(dat[, 2:3]) |> group_tt(i = dat$label)
 tab@output <- "markdown"
-expect_snapshot_print(tab, label = "group_tt-vector_row_labels.md")
+expect_snapshot_print(tab, label = "group-vector_row_labels.md")
 
 # 3 level tests across formats
 x <- mtcars[1:3, 1:5]
@@ -20,32 +20,32 @@ tab <- tt(x) |>
   group_tt(j = list("e" = 1:3, "f" = 4))
 
 t <- expect_table(tab, formats = c("markdown", "latex", "typst"))
-expect_snapshot_print(t[["markdown"]], "group_tt-3level.md")
-expect_snapshot_print(t[["latex"]], "group_tt-3level.tex")
-expect_snapshot_print(t[["typst"]], "group_tt-3level.typ")
+expect_snapshot_print(t[["markdown"]], "group-3level.md")
+expect_snapshot_print(t[["latex"]], "group-3level.tex")
+expect_snapshot_print(t[["typst"]], "group-3level.typ")
 
 tab <- tt(mtcars[1:10, 1:5]) |>
   group_tt(
     i = list("Hello" = 3, "World" = 8),
     j = list("Foo" = 2:3, "Bar" = 4:5)
   )
-expect_snapshot_print(print_html(tab), "group_tt-html_tutorial_01.html")
+expect_snapshot_print(print_html(tab), "group-html_tutorial_01.html")
 
 
 # Issue #258: group_tt indices with 0 and duplicates
 tab1 <- tt(head(iris)) |> group_tt(i = list("hello" = 1))
 t1 <- expect_table(tab1)
-expect_snapshot_print(t1[["latex"]], "group_tt-issue258_01_latex.tex")
-expect_snapshot_print(t1[["typst"]], "group_tt-issue258_01_typst.typ")
-expect_snapshot_print(t1[["markdown"]], "group_tt-issue258_01_markdown.md")
-expect_snapshot_print(t1[["html"]], "group_tt-issue258_01_html.html")
+expect_snapshot_print(t1[["latex"]], "group-issue258_01_latex.tex")
+expect_snapshot_print(t1[["typst"]], "group-issue258_01_typst.typ")
+expect_snapshot_print(t1[["markdown"]], "group-issue258_01_markdown.md")
+expect_snapshot_print(t1[["html"]], "group-issue258_01_html.html")
 
 tab2 <- tt(head(iris)) |> group_tt(i = list("hello" = 2, "world" = 2))
 t2 <- expect_table(tab2)
-expect_snapshot_print(t2[["latex"]], "group_tt-issue258_02_latex.tex")
-expect_snapshot_print(t2[["typst"]], "group_tt-issue258_02_typst.typ")
-expect_snapshot_print(t2[["markdown"]], "group_tt-issue258_02_markdown.md")
-expect_snapshot_print(t2[["html"]], "group_tt-issue258_02_html.html")
+expect_snapshot_print(t2[["latex"]], "group-issue258_02_latex.tex")
+expect_snapshot_print(t2[["typst"]], "group-issue258_02_typst.typ")
+expect_snapshot_print(t2[["markdown"]], "group-issue258_02_markdown.md")
+expect_snapshot_print(t2[["html"]], "group-issue258_02_html.html")
 
 # Issue #362: group_tt with duplicate column group labels in latex
 tab <- data.frame(
@@ -58,7 +58,7 @@ tab <- data.frame(
   tt() |>
   group_tt(j = list(a = 1:2, a = 3:4))
 tab@output <- "latex"
-expect_snapshot_print(tab, "group_tt-issue362_duplicate_colum_labels.tex")
+expect_snapshot_print(tab, "group-issue362_duplicate_colum_labels.tex")
 
 # Issue #413: Automatic row groups fail if column is a factor
 df <- mtcars |> head(10)
@@ -73,7 +73,7 @@ expect_inherits(save_tt(tab, "markdown"), "character")
 # No delimiter
 tab <- tt(mtcars |> head())
 tab@output <- "markdown"
-expect_snapshot_print(tab, "group_tt_delim-no-delim.md")
+expect_snapshot_print(tab, "group-delim-no-delim.md")
 tab <- tt(data.frame(
   A_id = 1,
   A_a1 = 2,
@@ -84,7 +84,7 @@ tab <- tt(data.frame(
 )) |>
   group_tt(j = "_")
 tab@output <- "markdown"
-expect_snapshot_print(tab, "group_tt_delim-all-delim.md")
+expect_snapshot_print(tab, "group-delim-all-delim.md")
 tab <- tt(data.frame(
   id = 1,
   A_a1 = 2,
@@ -95,7 +95,7 @@ tab <- tt(data.frame(
 )) |>
   group_tt(j = "_")
 tab@output <- "markdown"
-expect_snapshot_print(tab, "group_tt_delim-some-delim.md")
+expect_snapshot_print(tab, "group-delim-some-delim.md")
 tab <- tt(data.frame(
   id = 1,
   Axa1 = 2,
@@ -106,7 +106,7 @@ tab <- tt(data.frame(
 )) |>
   group_tt(j = "x")
 tab@output <- "markdown"
-expect_snapshot_print(tab, "group_tt_delim-x-delim.md")
+expect_snapshot_print(tab, "group-delim-x-delim.md")
 
 
 # Issue #466: group_tt without delimiter returns a valid table
@@ -126,7 +126,7 @@ tab <- tt(dat[, 2:3]) |>
   group_tt(i = dat$label) |>
   style_tt(i = "groupi", color = "white", background = "black", align = "c")
 tab@output <- "latex"
-expect_snapshot_print(tab, label = "group_tt_style_tt_group.tex")
+expect_snapshot_print(tab, label = "group_style_tt_group.tex")
 
 
 # Issue #165: Column labels not centered, last row not styled because of row group offset
@@ -139,7 +139,7 @@ tab <- tt(k) |>
   group_tt(i = list("hello world" = 2), j = list("foo" = 1:2, "bar" = 3:4))
 expect_snapshot_print(
   print_html(tab),
-  "group_tt-issue165_html_centering_style.html"
+  "group-issue165_html_centering_style.html"
 )
 
 
@@ -169,7 +169,7 @@ tab <- tt(head(iris)) |>
   style_tt("groupj", color = "orange") |>
   style_tt("colnames", color = "teal")
 tab <- expect_table(tab, "latex")[["latex"]]
-expect_snapshot_print(tab, "group_tt-style_tt_i_colnames_vs_groupj.tex")
+expect_snapshot_print(tab, "group-style_tt_i_colnames_vs_groupj.tex")
 
 
 # Issue #165: group_tt insert extra row at the bottom
