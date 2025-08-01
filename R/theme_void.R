d3ecee1980eced79638a638b85d088abde8232eb
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-01 18:12:07 -0400

    theme_void simplification

--- R/theme_void.R
+++ R/theme_void.R
@@ -17,18 +17,7 @@ finalize_theme_void <- function(table) {
 }
 
 theme_void <- function(x, ...) {
-  # placement function
-  place <- theme_placement_factory(
-    horizontal = get_option("tinytable_theme_default_horizontal", "c"),
-    latex_float = get_option(
-      "tinytable_theme_placement_latex_float",
-      default = NULL
-    )
-  )
-
   x <- style_tt(x, finalize = finalize_theme_void)
-  x <- style_tt(x, finalize = place)
   x <- theme_tt(x, "bootstrap", class = "table table-borderless")
-
   return(x)
 }
