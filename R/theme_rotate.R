882b8347243b4f81920f9317e41a3a46dafd0f1a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-01 18:14:24 -0400

    theme_rotate: simplify

--- R/theme_rotate.R
+++ R/theme_rotate.R
@@ -1,38 +1,45 @@
 theme_rotate <- function(x, angle = 90, ...) {
   assert_numeric(angle, len = 1, lower = 0, upper = 360)
+
+  # latex
   fn <- function(table) {
-    if (isTRUE(table@output == "latex")) {
-      rot <- sprintf("\\begin{table}\n\\rotatebox{%s}{", angle)
-      table@table_string <- sub(
-        "\\begin{table}",
-        rot,
-        table@table_string,
-        fixed = TRUE
-      )
-      table@table_string <- sub(
-        "\\end{table}",
-        "}\n\\end{table}",
-        table@table_string,
-        fixed = TRUE
-      )
-    } else if (isTRUE(table@output == "typst")) {
-      rot <- sprintf("#rotate(-%sdeg, reflow: true, [\n  #figure(", angle)
-      table@table_string <- sub(
-        "#figure(",
-        rot,
-        table@table_string,
-        fixed = TRUE
-      )
-      table@table_string <- sub(
-        ") // end figure",
-        ") ]) // end figure",
-        table@table_string,
-        fixed = TRUE
-      )
-    }
-    table <- style_tt(table, finalize = fn)
+    rot <- sprintf("\\begin{table}\n\\rotatebox{%s}{", angle)
+    table@table_string <- sub(
+      "\\begin{table}",
+      rot,
+      table@table_string,
+      fixed = TRUE
+    )
+    table@table_string <- sub(
+      "\\end{table}",
+      "}\n\\end{table}",
+      table@table_string,
+      fixed = TRUE
+    )
     return(table)
   }
-  x <- style_tt(x, finalize = fn)
+  attr(fn, "output") <- "latex"
+  x@lazy_finalize <- c(x@lazy_finalize, list(fn))
+
+  # typst
+  fn <- function(table) {
+    rot <- sprintf("#rotate(-%sdeg, reflow: true, [\n  #figure(", angle)
+    table@table_string <- sub(
+      "#figure(",
+      rot,
+      table@table_string,
+      fixed = TRUE
+    )
+    table@table_string <- sub(
+      ") // end figure",
+      ") ]) // end figure",
+      table@table_string,
+      fixed = TRUE
+    )
+    return(table)
+  }
+  attr(fn, "output") <- "typst"
+  x@lazy_finalize <- c(x@lazy_finalize, list(fn))
+
   return(x)
 }
