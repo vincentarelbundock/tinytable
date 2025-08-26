c3335a0fc531b38095659c9e406881d7a8fe7ee9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-26 06:15:10 -0400

    test

--- inst/tinytest/test-ansi.R
+++ inst/tinytest/test-ansi.R
@@ -1,8 +1,26 @@
 source("helpers.R")
-expect_false(TRUE)
-exit_file("do this")
+using("tinysnapshot")
+options(tinytable_print_output = "markdown")
 
-pkgload::load_all()
+data <- data.frame(
+  Name = c("Alice", "Bob", "Charlie"),
+  Age = c(25, 30, 35),
+  Score = c(95.5, 87.2, 92.8)
+)
+tab <- tt(data, caption = "Three friends.") |>
+  style_tt(i = c(0, 3), color = "orange") |>
+  style_tt(i = 1, background = "teal", color = "black", bold = TRUE) |>
+  style_tt(i = 2, j = 2, underline = TRUE, color = "red") |>
+  style_tt(i = 3, strikeout = TRUE) |>
+  group_tt(j = list("Characteristics" = 2:3)) |>
+  style_tt(i = "caption", bold = TRUE, color = "red") |>
+  style_tt(i = "notes", bold = TRUE, color = "red") |>
+  theme_markdown(ansi = TRUE, hline = FALSE)
+
+expect_snapshot_print(tab, label = "ansi-styles_01.md")
+
+
+exit_file("broken")
 data <- data.frame(
   Name = c("Alice", "Bob", "Charlie"),
   Age = c(25, 30, 35),
