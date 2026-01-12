cce3113e8ed81a81cc549aaca36823f4cec2df87
etiennebacher <etienne.bacher@protonmail.com>
2026-01-12 13:31:28 +0100

    init

--- R/template.R
+++ R/template.R
@@ -3,25 +3,4 @@ template_tabularray <- function(theme = "default") {
     system.file("templates/tabularray_default.tex", package = "tinytable")
   )
   return(out)
-  if (theme %in% c("default", "grid")) {
-    out <- readLines(
-      system.file("templates/tabularray_default.tex", package = "tinytable")
-    )
-  } else if (theme == "bootstrap") {
-    out <- readLines(
-      system.file("templates/tabularray_bootstrap.tex", package = "tinytable")
-    )
-  } else if (theme == "striped") {
-    out <- readLines(
-      system.file("templates/tabularray_default.tex", package = "tinytable")
-    )
-  } else if (theme == "void") {
-    out <- readLines(
-      system.file("templates/tabularray_void.tex", package = "tinytable")
-    )
-  } else if (theme == "grid") {
-    out <- readLines(
-      system.file("templates/tabularray_grid.tex", package = "tinytable")
-    )
-  }
 }
